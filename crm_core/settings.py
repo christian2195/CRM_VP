cb275e85c334750000e01f9a368ed783e64c55ea
christian2195 <52585369+christian2195@users.noreply.github.com>
2026-08-09 10:51:53 -0400

    Update settings.py

--- crm_core/settings.py
+++ crm_core/settings.py
@@ -1,6 +1,7 @@
 import os
+from dotenv import load_dotenv
 from pathlib import Path
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -69,11 +70,11 @@ WSGI_APPLICATION = 'crm_core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'crm_db'),
-        'USER': os.environ.get('DB_USER', 'crm_user'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'Tecno/*2025*'),
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
